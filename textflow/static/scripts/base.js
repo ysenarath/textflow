96aac580b7a2551053b6e7ff0ffb84e1db3596af
Yasas Senarath <ysenarath.93@gmail.com>
2020-12-08 13:07:13 -0500

    Fix: Add Navbar burger click events.

--- textflow/static/scripts/base.js
+++ textflow/static/scripts/base.js
@@ -34,3 +34,13 @@ document.addEventListener('keydown', function (event) {
         closeDropdowns();
     }
 });
+
+// https://bulma.io/documentation/components/navbar/
+$(document).ready(function () {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function () {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+});
