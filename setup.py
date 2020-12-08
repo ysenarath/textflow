a1be7775d1ed38fdcc9ae54ae2d403252d880387
Yasas Senarath <ysenarath.93@gmail.com>
2020-12-08 12:42:15 -0500

    Update setup configurations.

---
+++ setup.py
@@ -0,0 +1,29 @@
+""" Python PyPi Setup Configurations """
+
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read().strip()
+
+with open('packages.txt') as fh:
+    requirements = fh.read().strip().split('\n')
+
+setuptools.setup(
+    name="textflow",
+    version="0.0.1",
+    author="Yasas Senarath",
+    author_email="ysenarath.93@gmail.com",
+    description="Minimal Annotation Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ysenarath/textflow",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
