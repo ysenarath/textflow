abd547ef8738ea0f0d1a788b5867c4d3ad1b0208
Yasas Senarath <ysenarath.93@gmail.com>
2020-12-08 12:34:34 -0500

    Add example for new interface.

---
+++ examples/basic_server/server.py
@@ -0,0 +1,16 @@
+""" Project Server
+
+Serves the text annotation project.
+"""
+import json
+import os
+
+from textflow import TextFlow
+
+with open(os.path.join(os.getcwd(), 'config.json')) as fp:
+    config = json.load(fp)
+
+tf = TextFlow(config)
+
+if __name__ == '__main__':
+    tf.app.run(debug=True)
