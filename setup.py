0156ac1676d16c8eb686430e3ebf31995ad75e6f
Yasas Senarath <ysenarath.93@gmail.com>
2020-12-15 12:29:38 -0500

    Update version to v0.0.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('packages.txt') as fh:
 
 setuptools.setup(
     name="textflow",
-    version="0.0.6",
+    version="0.0.7",
     author="Yasas Senarath",
     author_email="ysenarath.93@gmail.com",
     description="Minimal Annotation Framework",
