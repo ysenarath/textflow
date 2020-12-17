59d123aef1261aa3337cd6e82b0f28ce1975a785
Yasas Senarath <ysenarath.93@gmail.com>
2020-12-17 03:46:51 -0500

    Add docstring for cls/func.

--- textflow/utils/jsend.py
+++ textflow/utils/jsend.py
@@ -1,4 +1,13 @@
+""" JSend Response formatting """
+
+
 def response(status, **kwargs):
+    """ Generic response format
+
+    :param status: status content
+    :param kwargs: other content
+    :return: dictionary with status and other
+    """
     reply = {'status': status, }
     for k, v in kwargs.items():
         reply[k] = v
@@ -6,16 +15,31 @@ def response(status, **kwargs):
 
 
 def success(data=None):
+    """ Json formatting for success message
+
+    :param data: data field
+    :return: Json Response
+    """
     if data is None:
         data = {}
     return response('success', data=data)
 
 
 def fail(data=None):
+    """ Json formatting for failed message
+
+    :param data: data field
+    :return: Json Response
+    """
     if data is None:
         data = {}
     return response('fail', data=data)
 
 
 def error(message='Unable to determine error. Please contact admin for fixing this issue.'):
+    """ Json formatting for error message
+
+    :param message: message field
+    :return: Json Response
+    """
     return response('error', message=message)
