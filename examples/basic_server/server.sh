3bc5dead31afe7d99b474e46e097cdfce76d0ad5
Yasas <ysenarath.93@gmail.com>
2021-02-10 22:14:36 -0500

    Add guest users to test dataset generation

--- examples/basic_server/server.sh
+++ examples/basic_server/server.sh
@@ -41,10 +41,21 @@ python -m textflow document upload -p 2 -i documents.json
 # create user named admin
 # for this example we use simple password
 python -m textflow user create -u admin -p admin@123
+python -m textflow user create -u guest1 -p guest1@123
+python -m textflow user create -u guest2 -p guest2@123
+python -m textflow user create -u guest3 -p guest3@123
 
-# assign user to project
+# assign user to project 1
 python -m textflow user assign -u admin -p 1 -r admin
+python -m textflow user assign -u guest1 -p 1
+python -m textflow user assign -u guest2 -p 1
+python -m textflow user assign -u guest3 -p 1
+
+# assign user to project 2
 python -m textflow user assign -u admin -p 2 -r admin
+python -m textflow user assign -u guest1 -p 2
+python -m textflow user assign -u guest2 -p 2
+python -m textflow user assign -u guest3 -p 2
 
 # now run the project using __main__.sh
 python app.py
