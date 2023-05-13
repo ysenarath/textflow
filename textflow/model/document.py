16c5b504ba7ade0df80c44eabbd016db3878ae56
Yasas <wayasas@gmailc.om>
2023-05-13 19:44:26 +0200

    Add `source_id` as an alternative for `id_str`.

--- textflow/model/document.py
+++ textflow/model/document.py
@@ -8,6 +8,8 @@ from textflow.services.base import database as db
 class Document(db.Model):
     """ Document Entity """
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # id_str is different from the id field because it is used to store the
+    # original id from the source file
     id_str = db.Column(db.String(128), nullable=True)
     text = db.Column(db.Text(), nullable=False)
     meta = db.Column(db.JSON, nullable=True)
@@ -21,3 +23,11 @@ class Document(db.Model):
 
     def __getitem__(self, item):
         return html.unescape(self.text.__getitem__(item))
+
+    @property
+    def source_id(self):
+        return self.id_str
+    
+    @source_id.setter
+    def source_id(self, value):
+        self.id_str = value
