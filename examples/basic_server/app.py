1c1a2548c40b2b82d785922280f7a9b5f15ed9f2
Yasas <wayasas@gmailc.om>
2023-05-13 19:44:51 +0200

    Add `create_app` function.

--- examples/basic_server/app.py
+++ examples/basic_server/app.py
@@ -10,7 +10,10 @@ from textflow import TextFlow
 with open(os.path.join(os.getcwd(), 'config.json')) as fp:
     config = json.load(fp)
 
-tf = TextFlow(config)
+tf = TextFlow(config, url_prefix='/textflow')
+
+def create_app():
+    return tf.app
 
 if __name__ == '__main__':
     tf.app.run(port=8002, debug=True)
