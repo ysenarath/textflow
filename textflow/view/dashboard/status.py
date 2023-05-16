64e4b02f71ecf5cbe43743eb307a492f2411eb85
Yasas <wayasas@gmailc.om>
2023-05-16 12:14:44 +0200

    Add endpoint to show task status.

--- textflow/view/dashboard/status.py
+++ textflow/view/dashboard/status.py
@@ -1,5 +1,6 @@
 """ project admin view """
 
+from celery.result import AsyncResult
 from flask import jsonify
 
 from textflow import auth, services
@@ -9,14 +10,27 @@ from textflow.view.base import FakeBlueprint
 view = FakeBlueprint()
 
 
-@view.route('/api/projects/<project_id>/status')
+@view.route('/api/status/projects/<project_id>')
 @auth.login_required
 @auth.roles_required(role=['admin', 'manager'])
-def get_status(project_id):
+def get_project_status(project_id):
     """Gets agreement scores for provided project
 
     :param project_id: project id
     :return: multiple types of score values
     """
     status = services.get_status(project_id=project_id)
+    return jsonify(jsend.success(status))
+
+
+@view.route('/api/status/tasks/<id>')
+@auth.login_required
+@auth.roles_required(role=['admin', 'manager'])
+def get_task_status(id: str) -> dict[str, object]:
+    result = AsyncResult(id)
+    status = {
+        'ready': result.ready(),
+        'successful': result.successful(),
+        'value': result.result if result.ready() else None,
+    }
     return jsonify(jsend.success(status))
